cba7b2cacd4b02403750dac4ee372dd2d3e3ab2a
Victor Italo <victoritalo@live.com>
2022-11-17 19:33:50 -0300

    ft

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ btnSenha.addEventListener('click', () => {
       }
     })
 
+    let email = document.querySelector('#email');
+    let emailUser = document.querySelector('#emailUser');
+    let password = document.querySelector('#password');
+    let passwordUser = document.querySelector('#passwordUser');
+
+    email.addEventListener('keyup', () =>{
+      if(email.value !== string){
+        emailUser.setAttribute('style', 'color: #ff4f61');
+
+      } else{
+        emailUser.setAttribute('style', 'color: #7f73a1');
+
+      }
+      
+    })
+
+
+
+password.addEventListener('keyup', () => {
+      passwordUser.style.fontSizeAdjust = ''
+      if(password.value.length <= 3){
+        passwordUser.setAttribute('style', 'color: red');
+        passwordUser.innerHTML = ('4 ou mais caracteres')
+      } else{
+        passwordUser.setAttribute('style', 'color: green');
+        passwordUser.innerHTML = '4 ou mais caracteres'
+        //fazer com small para fonte
+      }
+    })
