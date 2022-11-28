f97a697db9ea33ee9d5ed2628febf253cd08c9b1
Victor Italo <victoritalo@live.com>
2022-11-28 02:20:06 -0300

    ft update login

--- cadastrarUser/cadastro.js
+++ cadastrarUser/cadastro.js
@@ -20,46 +20,120 @@ btnSenha2.addEventListener("click", () => {
   }
 });
 
+document.querySelector("#btn_cadastro").addEventListener("click", cadastroUser);
 
-    let labelUserName = document.querySelector('#labelUserName');
-    let username = document.querySelector('#username');
-    
-    let email = document.querySelector('#email');
-    let labelEmailUser = document.querySelector('#labelEmailUser');
+// let labelUserName = document.querySelector('#labelUserName');
+// let username = document.querySelector('#username');
+// // let labelUserName = document.querySelector('#labelUserName');
+// let email = document.querySelector('#email');
+// let labelEmailUser = document.querySelector('#labelEmailUser');
 
-    let password = document.querySelector('#password');
-    let labelPassword = document.querySelector('#labelPassword');
-    
-    let password2 = document.querySelector('#password2');
-    let labelPassword2 = document.querySelector('#labelPassword2');
+// let password = document.querySelector('#password');
+// let labelPassword = document.querySelector('#labelPassword');
 
+// let password2 = document.querySelector('#password2');
+// let labelPassword2 = document.querySelector('#labelPassword2');
 
-    username.addEventListener('keyup', () =>{
-      if(username.value.length <= 4){
-        labelUserName.setAttribute('style', 'color: #ff715b');
+//alerta de cores para confirmar o tamanho do nome - Existe a validação na func cadastroUser
+username.addEventListener("keyup", () => {
+  if (username.value.length <= 4) {
+    labelUserName.setAttribute("style", "color: #ff715b");
+  } else {
+    labelUserName.setAttribute("style", "color: #7f73a1");
+  }
+});
 
-      } else{
-        labelUserName.setAttribute('style', 'color: #7f73a1');
-      }
-      
-    })
+password.addEventListener("keyup", () => {
+  if (password.value.length <= 5) {
+    labelPassword.setAttribute("style", "color: #ff715b");
+  } else {
+    labelPassword.setAttribute("style", "color: #7f73a1");
+  }
+});
 
-    password.addEventListener('keyup', () => {
-      if(password.value.length <= 5){
-        labelPassword.setAttribute('style', 'color: #ff715b');
-      } else{
-        labelPassword.setAttribute('style', 'color: #7f73a1');
-      }
+password2.addEventListener("keyup", () => {
+  if (password2.value !== password.value) {
+    labelPassword2.setAttribute("style", "color: #ff715b");
+    labelPassword2.innerHTML = "Senhas não combinam";
+  } else {
+    labelPassword2.setAttribute("style", "color: #7f73a1");
+    labelPassword2.innerHTML = "Senhas combinam!";
+  }
+});
 
-      
-    })
+function cadastroUser(event) {
+  event.preventDefault();
 
-    password2.addEventListener('keyup', () => {
-      if(password2.value !== password.value){
-        labelPassword2.setAttribute('style', 'color: #ff715b');
-        labelPassword2.innerHTML = ('Senhas não combinam')
-      } else{
-        labelPassword2.setAttribute('style', 'color: #7f73a1');
-        labelPassword2.innerHTML = 'Senhas combinam!'
-      }
-    })
+  const usuarioInput = document.getElementById("username");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const password2Input = document.getElementById("password2");
+  // let labelUserName = document.querySelector('#labelUserName');
+
+  if (passwordInput.value <= 5) {
+    labelPassword.setAttribute("style", "color: #ff715b");
+  }
+  if (usuarioInput.value <= 4) {
+    labelUserName.setAttribute("style", "color: #ff715b");
+  }
+  if (emailInput.value <= 4) {
+    labelEmailUser.setAttribute("style", "color: #ff715b");
+  }
+  if (passwordInput.value !== password2Input.value) {
+    let labelPassword2 = document.querySelector("#labelPassword2");
+    labelPassword2.setAttribute("style", "color: #ff715b");
+    labelPassword2.innerHTML = "Senhas não combinam";
+    return;
+  }
+
+  //verifica se todos os campos estao preenchidos
+  if (
+    !(usuarioInput.value !== "") ||
+    !(emailInput.value !== "") ||
+    !(passwordInput.value !== "") ||
+    !(password2Input.value !== "")
+  ) {
+    alert(`Existem campos em branco!`);
+    return;
+  }
+
+  // pega usuarios do localstorage
+  let users_bd = pegaUsers();
+
+  // verifica se ja existe um user com o email cadastrado
+  let usersFiltrado = users_bd.filter((user) => user.email_bd == emailInput.value);
+  console.log("filtro de email");
+  if (usersFiltrado.length > 0) {
+    labelEmailUser.innerHTML = "Este email já está cadastrado!";
+    alert('Este email já está em uso, por favor utilize outro email!')
+    return;
+  }
+
+  // cria um objeto do usuario
+  const usuario = {
+    nome_bd: usuarioInput.value,
+    email_bd: emailInput.value,
+    senha_bd: passwordInput.value,
+    mensagens_bd: [],
+  };
+
+  // adiciona ao array de usuarios
+  users_bd.push(usuario);
+  // salva no local storage
+  localStorage.setItem("users_bd", JSON.stringify(users_bd));
+  alert("Usuário criado com sucesso!");
+ 
+  setTimeout(function() {
+    window.location.href="../index.html";
+  }, 2000);
+}
+
+function pegaUsers() {
+  let usuarios = localStorage.getItem("users_bd");
+  if (!usuarios) {
+    usuarios = [];
+  } else {
+    usuarios = JSON.parse(usuarios);
+  }
+  return usuarios;
+}
--- index.js
+++ index.js
@@ -1,40 +1,83 @@
-let btnSenha = document.querySelector('.fa-eye')
-btnSenha.addEventListener('click', () => {
-    let inputSenha = document.querySelector('#password')
 
-    if(inputSenha.getAttribute('type') == 'password'){
-        inputSenha.setAttribute('type', 'text')
-      } else {
-        inputSenha.setAttribute('type', 'password')
-      }
-    })
+let inputEmail = document.querySelector("#email");
+let labelEmailUser = document.querySelector("#labelEmailUser");
+let inputPassword = document.querySelector("#password");
+let labelPasswordUser = document.querySelector("#labelPasswordUser");
+let labelErrorMsg = document.querySelector("#labelErrorMsg");
+let btnSenha = document.querySelector(".fa-eye");
+document.querySelector("#button_entrar").addEventListener("click", loginUser);
 
-    let email = document.querySelector('#email');
-    let emailUser = document.querySelector('#emailUser');
-    let password = document.querySelector('#password');
-    let passwordUser = document.querySelector('#passwordUser');
+function loginUser(event) {
+  event.preventDefault();
 
-    email.addEventListener('keyup', () =>{
-      if(email.value !== string){
-        emailUser.setAttribute('style', 'color: #ff4f61');
+  //verifica se todos os campos estao preenchidos
+  if (!(inputEmail.value !== "") || !(inputPassword.value !== "")) {
+    labelErrorMsg.innerHTML = "Preencha todos os campos!";
+    labelErrorMsg.setAttribute('style', 'color: #ff715b;');
+    inputEmail.focus();
+    return;
+  } 
 
-      } else{
-        emailUser.setAttribute('style', 'color: #7f73a1');
+  let users_bd = [];
+  let userValidator = {
+    nome: "",
+    email: "",
+    senha: "",
+    mensagem: "",
+  };
 
-      }
-      
-    })
+  users_bd = JSON.parse(localStorage.getItem("users_bd"));
 
+  users_bd.forEach((getbd) => {
+    if (
+      inputEmail.value == getbd.email_bd &&
+      inputPassword.value == getbd.senha_bd
+    ) {
+      userValidator = {
+        nome: getbd.nome_bd,
+        email: getbd.email_bd,
+        senha: getbd.senha_bd,
+        mensagem: getbd.mensagem_bd,
+      };
+    }
+  });
+ 
+  if(inputEmail.value != userValidator.email && inputPassword.value != userValidator.senha) {
+    labelErrorMsg.innerHTML = "Email ou senha incorretos!";
+    labelErrorMsg.setAttribute('style', 'color: #ff715b;');
+    inputEmail.focus();
+    return;
+  } else {
+    labelErrorMsg.innerHTML = "";
+    setTimeout(function() {
+      window.location.href="../CRUD/crudPage.html";
+    }, 2000);
+  }
+}
 
+  //------------------------------------------------------------------------------
+  //Altera cores da label como alerta visual @EMAIL INPUT
+  inputEmail.addEventListener("keyup", () => {
+    if (inputEmail.value.length <= 4) {
+      labelEmailUser.setAttribute("style", "color: #ff715b");
+    } else {
+      labelEmailUser.setAttribute("style", "color: #7f73a1");
+    }
+  });
+  //Altera cores da label como alerta visual @Password INPUT
+  inputPassword.addEventListener("keyup", () => {
+    if (password.value.length <= 5) {
+      labelPasswordUser.setAttribute("style", "color: #ff715b");
+    } else {
+      labelPasswordUser.setAttribute("style", "color: #7f73a1");
+    }
+  });
+  //Mostrar password
+  btnSenha.addEventListener("click", () => {
+    if (inputPassword.getAttribute("type") === "password") {
+      inputPassword.setAttribute("type", "text");
+    } else {
+      inputPassword.setAttribute("type", "password");
+    }
+  });
 
-password.addEventListener('keyup', () => {
-      passwordUser.style.fontSizeAdjust = ''
-      if(password.value.length <= 3){
-        passwordUser.setAttribute('style', 'color: red');
-        passwordUser.innerHTML = ('4 ou mais caracteres')
-      } else{
-        passwordUser.setAttribute('style', 'color: green');
-        passwordUser.innerHTML = '4 ou mais caracteres'
-        //fazer com small para fonte
-      }
-    })
