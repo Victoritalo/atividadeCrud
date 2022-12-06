8cc8f6b78ae925e21d7c401ab6f3a66b79f5e0ec
Victor Italo <victoritalo@live.com>
2022-12-06 20:02:31 -0300

    fix style positions

--- recadosUser/recados.js
+++ recadosUser/recados.js
@@ -1,23 +1,114 @@
-document.querySelector("#logout").addEventListener("click", logoutUser);
-let textinho = document.querySelector('#alteraTexto');
+
 let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
 let welcomeUser = document.querySelector('#welcomeUser');
+document.querySelector("#logout").addEventListener("click", logoutUser);
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const saveTitulo = document.querySelector('#m-titulo')
+const saveMensagem = document.querySelector('#m-mensagem')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+welcomeUser.innerHTML = `Olá, ${loggedUser.nome}!`
 
 if(localStorage.getItem('token') == null){
     alert("Realize o login para acessar a página!");
     document.querySelector('#hideall').style.visibility = 'hidden'
     setTimeout(function() {
         window.location.href="../index.html";
-      }, 1500);
+      }, 1000);
 }
-
-welcomeUser.innerHTML = `Bem vindo, ${loggedUser.nome}!`
 
 function logoutUser(){
     localStorage.removeItem('token');
-    // localStorage.removeItem('loggedUser');
+    localStorage.removeItem('loggedUser');
     window.location.href="../index.html";
 }
 
+let itens
+let id
 
+function openModal(edit = false, index = 0) {
+  modal.classList.add('active')
 
+  modal.onclick = e => {
+    if (e.target.className.indexOf('modal-container') !== -1) {
+      modal.classList.remove('active')
+    }
+  }
+
+  if (edit) {
+    saveTitulo.value = itens[index].titulo
+    saveMensagem.value = itens[index].mensagem
+
+    id = index
+  } else {
+    saveTitulo.value = ''
+    saveMensagem.value = ''
+  }
+  
+}
+
+function editItem(index) {
+
+  openModal(true, index)
+}
+
+function deleteItem(index) {
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td>${item.titulo}</td>
+    <td>${item.mensagem}</td>
+    <td></td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+  tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+  
+  if (saveTitulo.value == '' || saveMensagem.value == '') {
+    return
+  }
+
+  e.preventDefault();
+
+  if (id !== undefined) {
+    itens[id].titulo = saveTitulo.value
+    itens[id].mensagem = saveMensagem.value
+ 
+  } else {
+    itens.push({'titulo': saveTitulo.value, 'mensagem': saveMensagem.value})
+  }
+
+  setItensBD()
+
+  modal.classList.remove('active')
+  loadItens()
+  id = undefined
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
